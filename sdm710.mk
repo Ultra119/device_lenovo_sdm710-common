0e732a43f4748cab15550568ace6d8ff7a0daeed
Edwin Wong <edwinwong@google.com>
2021-03-25 14:36:24 -0700

    sdm710-common: Update drm HAL to v1.4
    Test: atest
      atest CtsMediaTestCases:android.media.cts.MediaDrmClearkeyTest#testGetPlaybackComponent
    
    Bug: 182525516
    Change-Id: I406b7d3c2921d4698535421894c6eb3785eb1c0d
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -187,6 +187,8 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.2.vendor \
     android.hardware.drm@1.3 \
     android.hardware.drm@1.3.vendor \
+	android.hardware.drm@1.4 \
+    android.hardware.drm@1.4.vendor \
     android.hardware.drm@1.4-service.clearkey
 
 # Gatekeeper
