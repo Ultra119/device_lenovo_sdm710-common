c5d38e75f8d0022f7165b946746451a949801e03
Ultra119 <pasasvistak0@gmail.com>
2022-05-07 20:06:38 +0300

    sdm710-common: Build libtinyxml.so
    F linker  : CANNOT LINK EXECUTABLE /vendor/bin/mm-pp-dpps: library libtinyxml.so not found

--- sdm710.mk
+++ sdm710.mk
@@ -426,6 +426,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
+# TinyXML
+PRODUCT_PACKAGES += \
+    libtinyxml
+
 # Tracing
 PRODUCT_PACKAGES += \
     android.hardware.atrace@1.0-service.pixel
